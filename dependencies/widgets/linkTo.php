cf8edec2e7ec7240b9285c49fb97e1a8884c40d1
Victor Francis <39800735+datruth14@users.noreply.github.com>
2025-09-25 13:51:04 +0100

    Update linkTo.php

--- dependencies/widgets/linkTo.php
+++ dependencies/widgets/linkTo.php
@@ -1,23 +1,6 @@
 <?php
-function linkTo(string $screen, string $params = "", string $text = null): string {
-    // normalize params: allow "&&a=1&&b=2" or "&a=1&b=2" or "a=1&b=2"
-    $params = trim($params);
-    if ($params === "") {
-        $qs = '?s=' . urlencode($screen);
-    } else {
-        // remove leading & or && and ensure it starts with &
-        $p = preg_replace('/^[&]+/', '', $params);
-        $p = '&' . $p;
-        $qs = '?s=' . urlencode($screen) . $p;
-    }
-
-    $attrs = "hx-get='{$qs}' hx-target='#appModalContent' hx-swap='innerHTML' hx-push-url='true' onclick=\"openModal()\"";
-
-    if ($text !== null) {
-        // return full <a> when text is provided
-        return "<a href='{$qs}' {$attrs}>{$text}</a>";
-    }
-
-    // else return attribute string to be injected inside button/div tags
-    return "{$attrs} data-linkto='{$qs}'";
+function linkTo(string $screen, string $params = ""): string {
+    $query = "?s={$screen}" . $params;
+    return "data-linkto='{$query}'";
 }
+?>
